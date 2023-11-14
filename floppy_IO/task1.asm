e64f9888c56465f790ae9143cecddac606aaf955
Sevastian Bajenov <sevabaj@gmail.com>
2023-11-14 14:39:47 +0200

    Add single delimiter write.

--- floppy_IO/task1.asm
+++ floppy_IO/task1.asm
@@ -2,11 +2,13 @@ org 7c00h
 
 section .data
     delimiter dd "@@@FAF-213 Sevastian BAJENOV###"
-    delimiter_length equ 42
     delimiter_count equ 10
     
     first_sector equ 13
     first_track equ 21
+
+    last_sector equ 6
+    last_track equ 23
 
 section .bss
     buffer resb 512
@@ -14,33 +16,15 @@ section .bss
 section .text
     global _start
 
+
 _start:
     mov ecx, 0
-    
-    jmp write_buffer
-
-write_buffer:
-    ; Copy one character from original string to repeated string
-    mov al, [original_string + esi]
-    mov [repeated_string + ecx], al
-
-    ; Increment indices
-    inc esi
-    inc ecx
-
-    ; Check if we reached the end of the original string
-    cmp byte [original_string + esi], 0
-    jnz repeat_loop
-
-    ; Check if we repeated the string ten times
-    cmp ecx, repeat_count * 10
-    je  end_program
-
-    ; If not, reset index for the original string and repeat
     mov esi, 0
-    jmp write_buffer
 
-write_delimiters:
+    jmp write_start_delimiter
+
+
+write_start_delimiter:
     ; set the floppy mode to write
     mov ah, 03h
     mov al, 1
@@ -50,9 +34,25 @@ write_delimiters:
     mov dh, 1        
     mov cl, first_sector
 
-    mov bx, buffer
+    mov bx, delimiter
 
     ; set the disk type to floppy
     mov dl, 0
+    int 13h
 
+
+write_end_delimiter:
+    ; set the floppy mode to write
+    mov ah, 03h
+    mov al, 1
+
+    ; set the address of the first sector to write
+    mov ch, last_track         
+    mov dh, 1        
+    mov cl, last_sector
+
+    mov bx, delimiter
+
+    ; set the disk type to floppy
+    mov dl, 0
     int 13h
