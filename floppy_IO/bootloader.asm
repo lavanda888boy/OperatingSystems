a77f5bf04bacc4b8eb11f4a36ae522c1fb2f2d1d
Sevastian Bajenov <sevabaj@gmail.com>
2023-11-30 10:03:22 +0200

    Add 'press any key to continue' in bootloader.

--- floppy_IO/bootloader.asm
+++ floppy_IO/bootloader.asm
@@ -17,8 +17,34 @@ mov dl, 0
 
 int 13h
 
+; print initial prompt
+mov si, prompt
+
+print_prompt:
+    mov al, [si]
+    cmp al, '$'
+    je end_print_prompt
+
+    mov ah, 0eh
+    int 10h
+    inc si
+    jmp print_prompt
+
+end_print_prompt:
+
+; read command option
+mov ah, 00h
+int 16h
+
+; display character as TTY
+mov ah, 0eh
+mov bl, 07h
+int 10h
+
 ; jump to the loaded NASM script
 jmp 0000h:1000h
+
+prompt db 'Press any key to continue: $'
 
 ; pad the bootloader to 510 bytes with zeros
 times 510 - ($ - $$) db 0
