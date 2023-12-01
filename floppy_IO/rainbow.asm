30cfc2f1103d88a334e74d11fef8970ef93cc151
Sevastian Bajenov <sevabaj@gmail.com>
2023-12-01 19:05:15 +0200

    Implement basic rainbow.

--- floppy_IO/rainbow.asm
+++ floppy_IO/rainbow.asm
@@ -1,10 +1,5 @@
 org 1000h
 
-section .data
-    video_mode db 13
-    pixel_color db 0
-    line_length db 0
-
 section .text
     global _start
 
@@ -14,19 +9,66 @@ _start:
     mov al, [video_mode]
     int 10h  
 
-    mov al, 4
-    mov cx, 10
+    ; red stripe
+    mov byte [stripes], 10
     mov byte [pixel_color], 4
-    mov byte [line_length], 20
-    call draw_line
+    call draw_stripe
+
+    ; orange stripe
+    mov byte [stripes], 10
+    mov byte [pixel_color], 12
+    call draw_stripe
+
+    ; yellow stripe
+    mov byte [stripes], 10
+    mov byte [pixel_color], 14
+    call draw_stripe
+
+    ; green stripe
+    mov byte [stripes], 10
+    mov byte [pixel_color], 10
+    call draw_stripe
+
+    ; light blue stripe
+    mov byte [stripes], 10
+    mov byte [pixel_color], 9
+    call draw_stripe
+
+    ; dark blue stripe
+    mov byte [stripes], 10
+    mov byte [pixel_color], 1
+    call draw_stripe
+
+    ; violet stripe
+    mov byte [stripes], 10
+    mov byte [pixel_color], 5
+    call draw_stripe
+
+
+draw_stripe:
+
+    stripe_loop:
+        mov byte [line_length], 240
+        mov cx, [left_indent]
+        call draw_line
+
+        cmp byte [stripes], 0
+        je end_stripe_loop
+
+        dec byte [stripes]
+        jmp stripe_loop
+
+    end_stripe_loop:
+
+    ret
 
 
 draw_line:
-    
+
     draw_pixel:
         mov ah, 0ch
-        mov al, [pixel_color]          
-        mov dx, 10
+        mov al, [pixel_color]
+        mov dx, [line_number]          
         int 10h
 
         inc cx
@@ -34,5 +76,18 @@ draw_line:
         dec byte [line_length]
         cmp byte [line_length], 0
         jne draw_pixel
+
+    inc word [line_number]
     
     ret
+
+end:
+
+
+section .data
+    video_mode db 13
+    pixel_color db 0
+    line_length db 0
+    left_indent dw 10
+    line_number dw 10
+    stripes db 0
